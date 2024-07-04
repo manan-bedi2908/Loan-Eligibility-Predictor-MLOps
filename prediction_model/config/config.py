384cf78131efe6f26113721c944b78da11dfef7b
manan-bedi2908 <mananbedilps@gmail.com>
2024-07-04 23:48:50 +0530

    data handling module updated

--- prediction_model/config/config.py
+++ prediction_model/config/config.py
@@ -9,6 +9,7 @@ DATAPATH = os.path.join(PACKAGE_ROOT, 'datasets')
 TRAIN_FILE = 'train.csv'
 TEST_FILE = 'test.csv'
 
+MODEL_NAME = 'Classifier.pkl'
 SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT, 'trained_models')
 
 TARGET = 'Loan_Status'
