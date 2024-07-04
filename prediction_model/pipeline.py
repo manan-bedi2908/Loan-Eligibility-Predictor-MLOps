5b3bdbf50226ba9df8328b6d813f799d9aef4c69
manan-bedi2908 <mananbedilps@gmail.com>
2024-07-05 00:16:02 +0530

    updated pipeline

---
+++ prediction_model/pipeline.py
@@ -0,0 +1,19 @@
+from sklearn.pipeline import Pipeline
+from prediction_model.config import config
+from prediction_model.processing import preprocessing as pp
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LogisticRegression
+
+classification_pipeline = Pipeline(
+    [
+        ('Mean Imputation', pp.MeanImputer(variables=config.NUM_FEATURES)),
+        ('Mode Imputation', pp.ModeImputer(variables=config.CAT_FEATURES)),
+        ('Domain Processing', pp.DomainProcessing(variable_to_modify=config.FEATURE_TO_MODIFY,
+                                                  variable_to_add=config.FEATURE_TO_ADD))
+        ('Drop Features', pp.DropColumns(variables_to_drop=config.DROP_FEATURES))
+        ('Label Encoding', pp.CustomLabelEncoder(variables=config.FEATURES_ENCODE))
+        ('Log Transformation', pp.LogTransforms(variables=config.LOG_FEATURES))
+        ('MinMaxSxale', MinMaxScaler()),
+        ('Logistic Regression Classifier', LogisticRegression(random_state=42))
+    ]
+)
