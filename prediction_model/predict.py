481f8f4e06fd5e0f6f2710d62de8d5712fac6318
manan-bedi2908 <mananbedilps@gmail.com>
2024-07-06 14:51:29 +0530

    updated tests

--- prediction_model/predict.py
+++ prediction_model/predict.py
@@ -15,19 +15,19 @@ from prediction_model.processing.data_handling import load_pipeline, load_datase
 
 classification_pipeline = load_pipeline(config.MODEL_NAME)
 
-# def generate_predictions(data_input):
-#     data = pd.DataFrame(data_input)
-#     pred = classification_pipeline.predict(data[config.FEATURES])
-#     output = np.where(pred == 1, 'Y', 'N')
-#     result = {"Predictions": output}
-#     return result
-
-def generate_predictions():
-    test_data = load_dataset(config.TEST_FILE)
-    pred = classification_pipeline.predict(test_data[config.FEATURES])
+def generate_predictions(data_input):
+    data = pd.DataFrame(data_input)
+    pred = classification_pipeline.predict(data[config.FEATURES])
     output = np.where(pred == 1, 'Y', 'N')
-    # result = {"Predictions": output}
-    return output
+    result = {"Predictions": output}
+    return result
+
+# def generate_predictions():
+#     test_data = load_dataset(config.TEST_FILE)
+#     pred = classification_pipeline.predict(test_data[config.FEATURES])
+#     output = np.where(pred == 1, 'Y', 'N')
+#     # result = {"Predictions": output}
+#     return output
 
 
 if __name__ == '__main__':
